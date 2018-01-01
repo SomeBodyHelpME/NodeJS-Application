fe24d4c1d4583808ff6616287dca719a78e5f69b
soobeeny <99xiubin@gamil.com>
2018-01-01 15:59:59 +0900

    show

--- routes/info/show.js
+++ routes/info/show.js
@@ -8,11 +8,13 @@ const db = require('../../module/pool.js');
 const sql = require('../../module/sql.js');
 
 router.get('/unperformed',async(res,req,next)=>{
+	console.log(req.headers);
 	console.log(req.header);
 	let token = req.header.token;
     let decoded = jwt.verify(token);
     let u_idx = decoded.u_idx;
-    console.log(decoded);
+    
+    console.log("decoded=",decoded);
 
     let result = await sql.findRestGroupThings(u_idx); 
     console.log(result);
@@ -24,7 +26,16 @@ router.get('/unperformed',async(res,req,next)=>{
 });
  
 router.get('/address',async(res,req,next)=>{
+	let token = req.header.token;
+    let decoded = jwt.verify(token);
+    let u_idx = decoded.u_idx;
+	let result = await sql.findAllGroupMemberAddr(u_idx);
+	 console.log(result);
 
+    res.status(200),send({
+    	message:"Success",
+    	data:result
+    });
 })
 
 module.exports = router;

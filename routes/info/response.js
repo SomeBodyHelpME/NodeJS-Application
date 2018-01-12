4c821e65e029d7d3ac66dca8df8232909d4b0847
이상은 <dltahazh@gmail.com>
2018-01-12 21:53:16 +0900

    small error modify_33 : Login Firebase Token modify_2

--- routes/info/response.js
+++ routes/info/response.js
@@ -106,7 +106,7 @@ router.post('/press', async(req, res, next) => {
     console.log(client_token);
     var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
         to: client_token,
-        notification: {
+        data: {
             title: '팀플의 요정',   //제목
             body: '투표 해주세요!!'  //보낼메시지
         }

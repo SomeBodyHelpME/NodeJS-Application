4eb01b8c389d242cdcce354ddb33232711b8c683
이상은 <dltahazh@gmail.com>
2018-03-12 19:06:12 +0900

    test : ActionVote method

--- routes/info/response.js
+++ routes/info/response.js
@@ -76,6 +76,8 @@ router.post('/vote', async(req, res, next) => {
         let vote_idx = req.body.vote_idx;
         let value = req.body.value;
 
+        console.log("vote_idx : " + vote_idx);
+        console.log("value : " + value);
         let write_time = moment().format("YYYY-MM-DD HH:mm:ss");
         let result = await sql.actionVote(u_idx, vote_idx, value, write_time);
         if(!result) {
